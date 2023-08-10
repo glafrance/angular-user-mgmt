1b337278ecf5e24909dbbd8007d86bfe347bcaf1
Gregory Lafrance <greglafrance007@gmail.com>
2023-08-10 13:25:46 -0400

    Initial code for an api endpoint to signup a user

--- server/app.js
+++ server/app.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
+
 const cors = require("cors");
 app.use(cors());
 
 const PORT = 4002;
 
-app.use("/", (req, res) => {
-  res.send("first simple api endpoint");
-});
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/usermgmt',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const usersRouter = require("./routes/users");
+
+app.use("/users", usersRouter);
 
 app.listen(PORT, () => {
   console.log(`User management server listening on port ${PORT}`);
---
+++ server/controllers/userController.js
@@ -0,0 +1,5 @@
+const User = require("../models/user");
+
+exports.signupUser = (req, res) => {
+  res.send("server signupUser");
+};
